4e7b11e5c7c422c70f2d95a8c414d6f56f2d340d
Eliardo Benoit <1086299@est.intec.edu.do>
2024-07-01 14:04:02 -0400

    Added tests

---
+++ tests/app.test.js
@@ -0,0 +1,36 @@
+const request = require('supertest');
+const express = require('express');
+const app = require('../app'); // Ajusta la ruta según sea necesario
+
+describe('GET /', () => {
+  it('should return 200 OK and render the index page', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toContain('Top 5 Trending Movies');
+  });
+});
+
+describe('GET /movies', () => {
+  it('should return 200 OK and render the movies page', async () => {
+    const res = await request(app).get('/movies');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toContain('Trending Movies');
+  });
+});
+
+describe('GET /series', () => {
+  it('should return 200 OK and render the series page', async () => {
+    const res = await request(app).get('/series');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toContain('Trending Series');
+  });
+});
+
+describe('GET /trailer', () => {
+  it('should return 302 and redirect if no title is provided', async () => {
+    const res = await request(app).get('/trailer');
+    expect(res.statusCode).toEqual(302);
+  });
+
+});
+
